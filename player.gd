994091a765c9d3378a729720beb668be11a9f134
EmilieDev <emilie.lebourgeois76@gmail.com>
2024-10-22 18:05:14 +0200

    animation inwork

--- player.gd
+++ player.gd
@@ -6,6 +6,8 @@ const FRICTION = 500
 
 var velocity_player = Vector2.ZERO
 
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,8 +15,11 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 		
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Idle/blend_position",input_vector)
 		velocity_player = velocity_player.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity_player = velocity_player.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = velocity_player
